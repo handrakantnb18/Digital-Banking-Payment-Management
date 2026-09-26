764739e26cf5c1636b06e4ba24360df7613ac11c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-26 21:22:06 +0530

    LoginRequest validation and private variables

---
+++ AuthService/src/main/java/com/digitalbank/AuthService/dto/LoginRequest.java
@@ -0,0 +1,51 @@
+package com.digitalbank.AuthService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 to 20 Charactors")
+    private String password;
+
+    public LoginRequest(){
+
+    }
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginResponse{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+
+}
